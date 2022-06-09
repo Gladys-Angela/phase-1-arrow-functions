f35e82334c38aa4dbca102523fba91b63d59d07b
Gladys <gladysangelakamau@gmail.com>
2022-06-09 15:53:08 +0300

    ph1arrowfunct

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const divide = (num1, num2) => 2000 / 100;
+ console.log(divide(2000,100));
+ 
+const square = (x)=>Math.pow(x, 2)
+ console.log(square)
+
+ const add = (a,b) => a + b;
+  add(3,4)
